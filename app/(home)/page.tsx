26d373aab3ca95bc82d6d7ed2f7bdcae6c2ffa79
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2025-04-23 00:32:50 -0500

    wip: main page design

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,18 @@
-import { User } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+"use client"
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 
 export default function HomePage() {
+const [active, setActive] = useState(false)
+
     return (
         <div>
-            <Avatar>
-                <AvatarImage src="/placeholder.svg?height=32&width=32" alt="User" />
-                <AvatarFallback>
-                    <User className="h-4 w-4" />
-                </AvatarFallback>
-            </Avatar>
+            <div className={`border ${active ?  "border-red-500 bg-red-500/10 p-3 text-red-700":"border-yellow-500 bg-yellow-500/10 p-3 text-yellow-700"} rounded`}>This is a trial run to check the buttons</div>
+            <Button className="rounded-full bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 my-10 cursor-pointer hover:opacity-95" onClick={()=>setActive(!active)}>
+                Toggle
+            </Button>
+            <div></div>
         </div>
     )
 }
